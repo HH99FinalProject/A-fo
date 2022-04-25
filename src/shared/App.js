8084e029983863004c2d9f705e4b4214299ba336
Yoonji Lee <ji0ijs2@naver.com>
2022-04-25 12:10:17 +0900

    globalstyle

--- src/shared/App.js
+++ src/shared/App.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import './App.css';
-import { ConnectedRouter } from "connected-react-router";
-import { Route } from "react-router-dom";
+import { ConnectedRouter } from 'connected-react-router';
+import { Route } from 'react-router-dom';
 import { history } from '../redux/configureStore';
-
 
 import Main from '../pages/Main';
 import Article from '../pages/Article';
@@ -13,17 +12,24 @@ import MyPage from '../pages/MyPage';
 import SubMain1 from '../pages/SubMain1';
 import SubMain2 from '../pages/SubMain2';
 
+import { Div, GlobalStyle } from '../components/ui';
+
 function App() {
   return (
-    <ConnectedRouter history={history}>
-      <Route exact path="/" component={Main} />
-      <Route exact path="/Article" component={Article} />
-      <Route exact path="/ArticleDetail" component={ArticleDetail} />
-      <Route exact path="/Detail" component={Detail} />
-      <Route exact path="/MyPage" component={MyPage} />
-      <Route exact path="/SubMain1" component={SubMain1} />
-      <Route exact path="/SubMain2" component={SubMain2} />
-    </ConnectedRouter>
+    <React.Fragment>
+      <GlobalStyle />
+      <Div container>
+        <ConnectedRouter history={history}>
+          <Route exact path="/" component={Main} />
+          <Route exact path="/Article" component={Article} />
+          <Route exact path="/ArticleDetail" component={ArticleDetail} />
+          <Route exact path="/Detail" component={Detail} />
+          <Route exact path="/MyPage" component={MyPage} />
+          <Route exact path="/SubMain1" component={SubMain1} />
+          <Route exact path="/SubMain2" component={SubMain2} />
+        </ConnectedRouter>
+      </Div>
+    </React.Fragment>
   );
 }
 
