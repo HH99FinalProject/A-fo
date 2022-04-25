c3d028b54f8ea559b2262be49a999c068203defc
Yoonji Lee <ji0ijs2@naver.com>
2022-04-25 17:02:14 +0900

    Div edit

--- src/shared/App.js
+++ src/shared/App.js
@@ -4,24 +4,26 @@ import { Route } from 'react-router-dom';
 import { history } from '../redux/configureStore';
 
 import Main from '../pages/Main';
-import Article from '../pages/Article';
-import ArticleDetail from '../pages/ArticleDetail ';
+import Board from '../pages/Board';
+import PostDetail from '../pages/PostDetail';
 import Detail from '../pages/Detail';
 import MyPage from '../pages/MyPage';
 import SubMain1 from '../pages/SubMain1';
 import SubMain2 from '../pages/SubMain2';
 
 import { Div, GlobalStyle } from '../components/ui';
+import { Header } from '../components/core';
 
 function App() {
   return (
     <React.Fragment>
       <GlobalStyle />
       <Div container>
+        <Header></Header>
         <ConnectedRouter history={history}>
           <Route exact path="/" component={Main} />
-          <Route exact path="/Article" component={Article} />
-          <Route exact path="/ArticleDetail" component={ArticleDetail} />
+          <Route exact path="/Board" component={Board} />
+          <Route exact path="/PostDetail" component={PostDetail} />
           <Route exact path="/Detail" component={Detail} />
           <Route exact path="/MyPage" component={MyPage} />
           <Route exact path="/SubMain1" component={SubMain1} />
