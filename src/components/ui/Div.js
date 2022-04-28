e0130f4e820f797305e81698c1bfc9fe0d8c6833
alonjoe <aalonjoe@gmail.com>
2022-04-28 11:45:41 +0900

    게시물작성,상세 1차 완료

--- src/components/ui/Div.js
+++ src/components/ui/Div.js
@@ -78,45 +78,6 @@ const Div = (props) => {
     velogCard,
     alignItems,
   };
-  if (bookMark) {
-    return (
-      <React.Fragment>
-        <Bookmark onClick={_onClick} {...styles}>
-          {children}
-        </Bookmark>
-      </React.Fragment>
-    );
-  }
-
-  if (modalContainer) {
-    return (
-      <React.Fragment>
-        <ModalContainer onClick={_onClick} {...styles}>
-          {children}
-        </ModalContainer>
-      </React.Fragment>
-    );
-  }
-
-  if (modalBox) {
-    return (
-      <React.Fragment>
-        <ModalBox onClick={_onClick} {...styles}>
-          {children}
-        </ModalBox>
-      </React.Fragment>
-    );
-  }
-
-  if (velogCard) {
-    return (
-      <React.Fragment>
-        <VelogCard onClick={_onClick} {...styles}>
-          {children}
-        </VelogCard>
-      </React.Fragment>
-    );
-  }
 
   return (
     <Box onClick={_onClick} {...styles}>
@@ -154,7 +115,7 @@ Div.defaultProps = {
   spaceBetween: false,
   spaceAround: false,
   textCenter: false,
-  fontSize: 'false',
+  fontSize: '',
   inline: false,
   bookMark: false,
   modal: false,
@@ -213,57 +174,5 @@ const Box = styled.div`
   }
 `;
 
-const Bookmark = styled.div`
-  position: absolute;
-  top: 0;
-  right: 24px;
-  display: block;
-  width: 32px;
-  height: 48px;
-`;
-
-const ModalContainer = styled.div`
-  position: fixed;
-  top: 0;
-  right: 0;
-  bottom: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background-color: rgba(0, 0, 0, 0.4);
-  border-radius: 10px;
-  z-index: 9999;
-`;
-
-const ModalBox = styled.div`
-  position: absolute;
-  left: 50%;
-  top: 50%;
-  display: flex;
-  width: 1000px;
-  height: 700px;
-  padding: auto 0px;
-  transform: translate(-50%, -50%);
-  background: #fff;
-  border-radius: 10px;
-`;
-
-const VelogCard = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  width: 320px;
-  height: 400px;
-  margin: 16px;
-  border-radius: 4px;
-  background-color: #f8f9fa;
-  box-shadow: rgb(0 0 0 / 7%) 0px 4px 16px 0px;
-  transition: box-shadow 0.25s ease-in 0s, transform 0.25s ease-in 0s;
-  overflow: hidden;
-  &:hover {
-    transform: translateY(-8px);
-    box-shadow: rgb(0 0 0 / 11%) 0px 12px 20px 0px;
-  }
-`;
 
 export default Div;
