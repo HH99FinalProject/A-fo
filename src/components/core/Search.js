c539c55f89fce6665b9143608833e79703367608
alonjoe <aalonjoe@gmail.com>
2022-04-28 19:28:36 +0900

    검색창 컴포넌트로

--- src/components/core/Search.js
+++ src/components/core/Search.js
@@ -3,7 +3,27 @@ import React from 'react';
 import { Button, Div, Image, Input, Text } from '../ui';
 
 const Search = () => {
-  return <Div>검색창입니다.</Div>;
+  return (
+    <Div width="1400px" margin="20px auto 50px" border="1px solid #000" padding="20px" alignItems="center">
+      <select>
+        <option>대륙</option>
+        <option>아시아</option>
+        <option>유럽</option>
+        <option>오세아니아</option>
+        <option>남아메리카</option>
+        <option>북아메리카</option>
+      </select>
+      <select>
+        <option>목적</option>
+        <option>워홀</option>
+        <option>이민</option>
+        <option>취업</option>
+        <option>유학</option>
+      </select>
+      <input style={{width: '80%', height:"20px", display: 'inline-block'}} placeholder="검색어를 입력해주세요"/>
+      <Button backgroundColor="skyblue" width="60px" border="1px solid #eee">검색</Button>
+    </Div>
+  )
 };
 
 export default Search;
--- src/pages/PostDetail.js
+++ src/pages/PostDetail.js
@@ -3,29 +3,12 @@ import styled from 'styled-components';
 import { history } from '../redux/configureStore';
 
 import { Div, Button, Text } from '../components/ui';
+import Search from '../components/core/Search';
 
 const PostDetail = () => {
   return (
     <React.Fragment>
-      <Div width="1400px" margin="20px auto 50px" border="1px solid #000" padding="20px" alignItems="center">
-        <select>
-          <option>대륙</option>
-          <option>아시아</option>
-          <option>유럽</option>
-          <option>오세아니아</option>
-          <option>남아메리카</option>
-          <option>북아메리카</option>
-        </select>
-        <select>
-          <option>목적</option>
-          <option>워홀</option>
-          <option>이민</option>
-          <option>취업</option>
-          <option>유학</option>
-        </select>
-        <input style={{width: '80%', height:"20px", display: 'inline-block'}} placeholder="검색어를 입력해주세요"/>
-        <Button backgroundColor="skyblue" width="60px" border="1px solid #eee">검색</Button>
-      </Div>
+      <Search />
       <Div width="1400px" margin="80px auto 50px"padding="20px 0" alignItems="center">
         <Text textAlign="center" size="36px" bold >자유 게시판</Text>
         <ReturnBtn onClick={() => { history.push("/board") }}>목록으로</ReturnBtn>
