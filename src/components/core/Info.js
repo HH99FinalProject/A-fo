fa952acc676577921dfcf87eacc8bd8ab3f69811
Yoonji Lee <ji0ijs2@naver.com>
2022-05-21 10:49:50 +0900

    에러 조건문

--- src/components/core/Info.js
+++ src/components/core/Info.js
@@ -12,9 +12,15 @@ const Info = (props) => {
   // 나라별 데이터
   const vCountry = useSelector((state) => state.country.vCountry);
   const countryDetail = useSelector((state) => state.country.countryList);
-  const countryInfo = countryDetail?.map((x, i) => x.BaseInfo.baseInfo);
+  const _countryInfo = countryDetail?.map((x, i) => x.BaseInfo.baseInfo);
 
-  React.useEffect(() => {});
+  React.useEffect(() => {
+    if (props.bank) {
+      props.countryInfo = undefined
+        ? (props.countryInfo = _countryInfo[0])
+        : '';
+    }
+  }, []);
 
   return (
     <>
